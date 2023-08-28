da89ab9f1653d1c9161409852ceffb4eba72ac1d
Martin Kustra <martin.kustra@tutanota.com>
2023-08-28 10:57:06 +0200

    Revert "Add errors to test CI pipeline"
    This reverts commit c519702cea04fb673fd3a53d62c03ca05f40c7f0.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,8 @@ use std::{env, process};
 use argon2_rs::{config::LocalConfig, hasher};
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1)
-                                       .collect();
+    let args: Vec<String> = env::args().skip(1).collect();
     let config = LocalConfig::from_args(&args);
-
-    let unused_variable = "hehe";
 
     // print!("{:#?}", config);
 
